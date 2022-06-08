ce5179af31a676e7ccb92a0e8e73440024eba3d5
Maxi Quoß <maxi@quoss.org>
2022-06-09 00:07:52 +0200

    soyifing live alert box

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,10 +19,35 @@
 
 <main class="flex-shrink-0">
     <div class="container">
-        {#if statsDB?.is_live}
-            <div class="alert alert-success" role="alert">
-                <strong>Max ist live!</strong> Hier gehts zum Stream:
-                <a href="https://www.twitch.tv/wubbl0rz" class="alert-link">twitch.tv/wubbl0rz</a>
+        {#if !statsDB?.is_live}
+            <div
+                class="alert alert-custom border-0 rounded-3 fs-4 m-2 mb-5 fw-bold text-center"
+                role="alert"
+            >
+                <p class="m-0 fs-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        fill="currentColor"
+                        class="bi bi-twitch me-2"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0H3.857zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142v6.286z"
+                        />
+                        <path
+                            d="M11.857 3.143h-1.143V6.57h1.143V3.143zm-3.143 0H7.571V6.57h1.143V3.143z"
+                        />
+                    </svg>
+                    Max ist live!
+                </p>
+                <p class="m-0">
+                    Hier gehts zum Stream: <a
+                        href="https://www.twitch.tv/wubbl0rz"
+                        class="alert-link">twitch.tv/wubbl0rz</a
+                    >
+                </p>
             </div>
         {/if}
         <h1 class="display-4 fw-bolder p-0 m-0 mb-4 align-self-center">
@@ -45,3 +70,24 @@
         {/await}
     </div>
 </main>
+
+<style lang="scss">
+    .alert-custom {
+        background-color: var(--color-hover);
+        color: var(--color-alert-text);
+        box-shadow: 7px 5px 0px var(--color-alert-text);
+
+        .alert-link {
+            color: var(--color-alert-text);
+        }
+    }
+
+    @media only screen and (min-width: 768px) {
+        .alert-custom {
+            background-image: url('/img/soyjak1.png'), url('/img/soyjak2.png');
+            background-position: left, right;
+            background-size: contain;
+            background-repeat: no-repeat;
+        }
+    }
+</style>
